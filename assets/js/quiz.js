c6bc5205d8344d788e7009ccd833ea49dcbca916
RH-devs <rasheedatajiborisha@gmail.com>
2021-09-06 09:22:23 +0000

    Add styles to fotter section

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -156,15 +156,29 @@ options.forEach(option => {
 
 		let optionToApply = selectedResult == currentQuestion.correctAnswer ? 'rightOption' : 'wrongOption'
 		
-          selectedOption.parentElement.classList.add(optionToApply); 
-
+          selectedOption.parentElement.classList.add(optionToApply);// Code adds the correct answer to the parent element when the user clicks on the correct answer.
 		setTimeout(() => {
 			selectedOption.parentElement.classList.remove(optionToApply);
 			getNewQuestion();
 
+          let finalScoreCounter = 0;  
           if (optionToApply === 'rightOption') {
                     incrementScore(SCORE_POINTS);
-               }
+                    finalScoreCounter += score;
+          } else {
+          goodCredit.innerText = ``;
+          finalScoreCounter += score;
+              }   
+
+     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+ 
+     if (finalScoreCounter > 500 && finalScoreCounter <= 1000) {
+                   goodCredit.innerText = `Good Job Fam😊!!! Your Total score is ${score} out of ${totalScoreofPercentGrade}, You are an Executive!`;
+     } else { //less than or equal to 500
+                   badCredit.innerText = `Awwww, you lost Fam!!! Your Total score is ${score} out of ${totalScoreofPercentGrade}, You can be Better Next Time!`;
+     }
+   
+}
           
 
 		}, 1000);
